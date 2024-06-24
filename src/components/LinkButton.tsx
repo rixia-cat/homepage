a28729b862e658702a95636eca1203a6202d7b23
rixia-cat <rixiacat@gmail.com>
2024-06-25 01:47:39 +0900

    💄 change: ライトモード時に見えなくなる点を修正

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -12,7 +12,7 @@ export default function LinkButton(props: LinkButtonProps) {
     const iconHorizontalMargin = iconPosition === "left" ? "mr-2" : "ml-2";
     const clonedClassAddedIcon = props.icon ? cloneElement(props.icon, { size: "1.5rem", className: `  ${iconHorizontalMargin}` }) : null;
     return (
-        <a href={props.href} className="flex items-center justify-center  text-base  outline outline-2 outline-gray-300 dark:outline-gray-300 text-gray-100 px-4 py-2 rounded-full"
+        <a href={props.href} className="flex items-center justify-center  text-base font-medium outline outline-1 outline-gray-600 dark:outline-gray-300 text-gray-900 dark:text-gray-100 px-4 py-2 rounded-full"
         >
             {iconPosition === "left" && clonedClassAddedIcon}
             <span   >{props.label}</span>
