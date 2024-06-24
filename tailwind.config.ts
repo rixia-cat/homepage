a5b9f9f684949fe72cab7daa774e730d86d9068e
rixia-cat <rixiacat@gmail.com>
2024-06-24 21:46:46 +0900

    💄 add: UI関連のライブラリ追加等

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       backgroundImage: {
@@ -15,6 +16,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-radix")(),
+  ],
 };
 export default config;
