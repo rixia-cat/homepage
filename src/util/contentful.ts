d6b1a790acde7a04dfe2e524c0a5956d042fd9bb
rixia <rixiacat@gmail.com>
2024-07-08 10:36:04 +0000

    🚑 fix: 判定ミスを修正

--- src/util/contentful.ts
+++ src/util/contentful.ts
@@ -1,7 +1,7 @@
 import "server-only";
 import { createClient } from "contentful";
 
-const IS_PRODUCTION = process.env.CONTENTFUL_IS_PREVMODE === "true";
+const IS_PRODUCTION = process.env.CONTENTFUL_IS_PREVMODE !== "true";
 export const contentfulClient = createClient({
   space: process.env.CONTENTFUL_SPACE_ID ?? "",
   accessToken: (IS_PRODUCTION ? process.env.CONTENTFUL_DELIVERY_TOKEN : process.env.CONTENTFUL_PREVIEW_TOKEN) ?? "",
